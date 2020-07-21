4acec297ee1d71adcf48624c8ccb5cb87c53e964
gyu <kai9702@gmail.com>
2020-07-22 01:11:15 +0900

    sum of n1 to n2

---
+++ Section1/Pro2/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int n1, n2;
+	int sum = 0;
+	
+	cin >> n1 >> n2;
+	
+	while(n1 <= n2){
+		cout << n1 << " ";
+		sum += n1;
+		if(n1 == n2){
+			cout << "= ";
+		}
+		else{
+			cout << "+";
+		}
+		n1++;
+	}
+	cout << sum;
+	return 0;
+}
