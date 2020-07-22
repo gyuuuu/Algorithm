aec3d08c68919cced2bd56e6ae6613d3994804cc
gyu <kai9702@gmail.com>
2020-07-22 19:12:13 +0900

    difference of age _ optimize

--- Section1/Pro4/main.cpp
+++ Section1/Pro4/main.cpp
@@ -3,24 +3,20 @@
 using namespace std;
 
 int main() {
-	int n;
+	int n, age;
+	int max = 0;
+	int min = 100;
 	
 	cin >> n;
-	
-	int* ageptr = new int[n];
-	
-	for(int i=0; i<n; i++){
-		cin >> ageptr[i];
-	}
-	
-	int min = ageptr[0];
-	int max = ageptr[0];
 		
 	for(int i=0; i<n; i++){
-		if(ageptr[i] > max)
-			max = ageptr[i];
-		if(ageptr[i] < min)
-			min = ageptr[i];
+		
+		cin >> age;
+		
+		if(age > max)
+			max = age;
+		if(age < min)
+			min = age;
 	}
 	
 	cout << max-min;
