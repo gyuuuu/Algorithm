94b24931277c43b1be13c0e19d1542014b58a532
gyu <kai9702@gmail.com>
2020-08-04 18:13:41 +0900

    22. 온도의 최댓값_use std::vector

--- Section1/Pro22/main.cpp
+++ Section1/Pro22/main.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
 #include <stdio.h>
+#include <vector>
 using namespace std;
 
 int main() {
 	//freopen("input.txt", "rt", stdin);
 	
 	int n,k, result;
-	int tem[100000];
 	int max = -24700000;
 	int sum=0;
 	scanf("%d %d", &n, &k);
 	
+	vector<int> tem(n);
 	int c = 0;
 	for(int i=0; i<n; i++){
 		scanf("%d", &tem[i]);
