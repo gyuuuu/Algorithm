d8a3928f3eb46766bd155a46662d3bb69a871baa
gyuuuu <kai9702@gmail.com>
2020-12-18 23:32:27 +0900

    edit

--- Section5/Pro4/main.cpp
+++ Section5/Pro4/main.cpp
@@ -5,16 +5,12 @@
 using namespace std;
 
 int main(int argc, char** argv) {
-	freopen("input.txt", "rt", stdin);
+	//freopen("input.txt", "rt", stdin);
 	int n;
 	cin >> n;
 	vector<int> arr(n);
 	for(int i=0; i<n; i++){
 		scanf("%d", &arr[i]);
-	}
-	
-	for(auto &ele : arr){
-		cout << ele << " ";
 	}
 	
 	return 0;
