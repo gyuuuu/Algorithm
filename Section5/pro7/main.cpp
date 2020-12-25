5c8dc109296042fb860f3d6d06769c9e10027bc0
gyuuuu <kai9702@gmail.com>
2020-12-25 23:24:21 +0900

    7. 알리바바와 40인의 도둑(bottom up)

--- Section5/pro7/main.cpp
+++ Section5/pro7/main.cpp
@@ -1,18 +1,27 @@
 #include <iostream>
 #include <vector>
+#include <climits>
 
 using namespace std;
 
 int main(int argc, char** argv) {
-	freopen("input.txt", "rt", stdin);
+	//freopen("input.txt", "rt", stdin);
 	int n;
 	cin >> n;
-	vector<vector<int> > matrix(n, vector<int>(n));
-	for(int i=0; i<n; i++){
-		for(int j=0; j<n; j++){
+	vector<vector<int> > matrix(n+2, vector<int>(n+2));
+	for(int i=1; i<=n; i++){
+		for(int j=1; j<=n; j++){
 			cin >> matrix[i][j];
 		}
 	}
-	
+	vector<vector<int> > dp(n+2, vector<int>(n+2, INT_MAX/2));
+	dp[1][1] = matrix[1][1];
+	for(int i=1; i<=n; i++){
+		for(int j=1; j<=n; j++){
+			if(i==1 && j==1) continue;
+			dp[i][j] = min(dp[i-1][j]+matrix[i][j], dp[i][j-1]+matrix[i][j]);
+		}
+	}
+	cout << dp[n][n];
 	return 0;
 }
