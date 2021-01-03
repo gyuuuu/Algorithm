da26cdc516cb1114a45618c5fee314125c602186
gyuuuu <kai9702@gmail.com>
2021-01-03 23:10:58 +0900

    change

--- Section5/Pro9/main.cpp
+++ Section5/Pro9/main.cpp
@@ -24,7 +24,7 @@ int main(int argc, char** argv) {
 //	freopen("input.txt", "rt", stdin);
 	int n;
 	cin >> n;
-//	vector<vector<int> > matrix(n, vector<int>(n));
+
 	for(int i=0; i<n; i++){
 		for(int j=0; j<n; j++){
 			cin >> matrix[i][j];
