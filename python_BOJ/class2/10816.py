cbad06e4f3e6a37bc7a7d17fe74ac5d98090eeb6
gyuuuu <kai9702@gmail.com>
2021-09-07 15:03:30 +0000

    숫자카드2 코드 수정

--- python_BOJ/class2/10816.py
+++ python_BOJ/class2/10816.py
@@ -1,31 +1,14 @@
 import sys
 from collections import Counter
 
-def bn_search(num, arr):
-  s = 0
-  e = len(arr)-1
-  index = -1
-  while s <= e:
-    mid = (s+e) // 2
-    if arr[mid] == num:
-      index = mid
-      break
-    elif arr[mid] < num:
-      s = mid + 1
-    else:
-      e = mid -1
-  return index
-
 n = int(sys.stdin.readline().rstrip())
-sangun = sorted(list(map(int, sys.stdin.readline().rstrip().split())))
-counter = Counter(sangun)
+sangun = Counter(list(map(int, sys.stdin.readline().rstrip().split())))
 m = int(sys.stdin.readline().rstrip())
 cards = list(map(int, sys.stdin.readline().rstrip().split()))
 cnts = [0 for i in cards]
 
 for i, card in enumerate(cards):
-  index = bn_search(card, sangun)
-  if counter.get(card) != None:
-    cnts[i] += counter.get(card)
+  if card in sangun:
+    cnts[i] += sangun.get(card)
 for c in cnts:
   print(c, end=' ')
